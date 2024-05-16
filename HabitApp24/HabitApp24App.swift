20b34636d635e290c970172a0f17e98cb70300b9
Гамид Халидов <khalidov-gamid@mail.ru>
2024-05-16 17:20:18 +0300

    Initial Commit

---
+++ HabitApp24/HabitApp24App.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp24App.swift
+//  HabitApp24
+//
+//  Created by Gamıd Khalıdov on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
